39ac60f35022083de4ed42f5e12a2e72ab1f0ee0
Rwan Abdelmonem <rabdelmonem0@gmail.com>
2021-02-22 20:29:30 -0800

    modified js code 2

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,27 @@
 // Assignment Code
 // Character variables 
+// var pass = randomPassword.join(""); 
+var possibelChars = [];
+var result = [];
+
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var numChar;
-var specialChar;
-var upperCaseChar;
-var lowerCaseChar;
+// var numChar;
+// var specialChar;
+// var upperCaseChar;
+// var lowerCaseChar;
 
 function generatePassword(){
   var passwordOptions = generatePasswordOptions();  
   // empty array to push final password characters into
-  var result = [];
+  
 
   // variable to store ALL the possible characters - starts as empty array
-  var possibelChars = [];
+
+    
   // variable to store an array that has one of each type chosen to - initialized as empty array
   // check the passwordOptions to see what characters the user wants
     // if user wants uppercase, possibleCharacters = possibleCharacters.concat(uppercase)
@@ -53,10 +58,10 @@ function generatePasswordOptions() {
     return;
   }
 
-  var numbers = confirm("Would you like numbers?");
-  var specialCharacters = confirm("Would you like special characters?");
-  var upperCase = confirm("Would you like uppercase letters?");
-  var lowerCase = confirm("would you like lowercase letters?");
+  var numChar = confirm("Would you like numbers?");
+  var specialChar = confirm("Would you like special characters?");
+  var upperCaseChar = confirm("Would you like uppercase letters?");
+  var lowerCaseChar = confirm("would you like lowercase letters?");
 
   // Check if all the prompts false -> then alert that password must contain at least one character type. Then return to go through again
   if (!numbers && !specialCharacters && !upperCase && !lowerCase) {
@@ -76,43 +81,48 @@ function generatePasswordOptions() {
   // if (lowerCaseChar) {
   //   passwordOptions = lowerCase
   // }
-
+  var passwordOptions = {
+    hasSpecialCharacters: specialChar, 
+    hasNumbers: numChar,
+    hasUpperCase: upperCaseChar,
+    hasLowerCase: lowerCaseChar,
+    length: passwordLength,
+  }
+  console.log(passwordOptions);
+  
   if (passwordOptions.hasNumbers) {
     possibelChars.concat(numbers)
-    result.push(numbers[randomNumber])
+    console.log(numbers)
+    result.push(getRandom(numbers))
   }
   if (passwordOptions.hasSpecialCharacters) {
     possibelChars.concat(specialCharacters)
-    result.push(specialCharacters[randomNumber])
+    result.push(getRandom(specialCharacters))
   }
   if (passwordOptions.hasLowerCase) {
     possibelChars.concat(numbers)
-    result.push(lowerCase[randomNumber])
+    result.push(getRandom(lowerCase))
   }
   if (passwordOptions.hasUpperCase) {
     possibelChars.concat(specialCharacters)
-    result.push(upperCase[randomNumber])
+    result.push(getRandom(upperCase))
   }
   // store passwordOptions in a variable
-  var passwordOptions = {
-    hasSpecialCharacters: specialCharacters, 
-    hasNumbers: numbers,
-    hasUpperCase: upperCase,
-    hasLowerCase: lowerCase,
-    length: passwordLength,
-  }
+ 
 
-  console.log(passwordOptions);
+  
   // return passwordOptions so it's usable outside this function scope
 }
 
 // Function to get random element from array
-function getRandom(passwordOptions) {
-  var randomIndex = Math.floor(Math.random() * passwordOptions.length);
-  var randomEl = passwordOptions[randomIndex];
-
+function getRandom(array) {
+  console.log(array);
+  var randomIndex = Math.floor(Math.random() * array.length);
+  console.log(randomIndex);
+  var randomEl = array[randomIndex];
+console.log(randomEl);
   return randomEl;
-// }
+}
 // for (var i = 0; i < passwordLength; i++) {
 //   var randomChar = passwordOptions[Math.floor(Math.random() * passwordOptions.length)];
 //   randomResult.push(randomChar);
@@ -120,10 +130,12 @@ function getRandom(passwordOptions) {
 
 
 var generateBtn = document.querySelector("#generate");
+  
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePasswordOptions();
+  console.log(result);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
